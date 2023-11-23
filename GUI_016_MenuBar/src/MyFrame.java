fa4840fd744bf6064fd1f473ee5f43e81d4a5f87
Afonso <67978137+AfonsoPaula@users.noreply.github.com>
2023-11-23 20:48:26 +0000

    AP: commit 8 - progressBar and menuBar

---
+++ GUI_016_MenuBar/src/MyFrame.java
@@ -0,0 +1,80 @@
+import java.awt.FlowLayout;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MyFrame extends JFrame implements ActionListener{
+	
+	JMenuBar menuBar;
+	
+	JMenu fileMenu;
+	JMenu editMenu;
+	JMenu helpMenu;
+	
+	JMenuItem loadItem;
+	JMenuItem saveItem;
+	JMenuItem exitItem;
+	
+	ImageIcon loadIcon;
+	ImageIcon saveIcon;
+	ImageIcon exitIcon;
+
+	MyFrame(){
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(500,500);
+		this.setLayout(new FlowLayout());
+		
+		loadIcon = new ImageIcon("file.png");
+		saveIcon = new ImageIcon("save.png");
+		exitIcon = new ImageIcon("exit.png");
+		
+		menuBar = new JMenuBar();
+		
+		fileMenu = new JMenu("File");
+		editMenu = new JMenu("Edit");
+		helpMenu = new JMenu("Help");
+		
+		loadItem = new JMenuItem("Load");
+		saveItem = new JMenuItem("Save");
+		exitItem = new JMenuItem("Exit");
+		
+		loadItem.addActionListener(this);
+		saveItem.addActionListener(this);
+		exitItem.addActionListener(this);
+		
+		loadItem.setIcon(loadIcon);
+		saveItem.setIcon(saveIcon);
+		exitItem.setIcon(exitIcon);
+		
+		fileMenu.setMnemonic(KeyEvent.VK_F); //Alt + F for file
+		editMenu.setMnemonic(KeyEvent.VK_E); //Alt + E for edit
+		helpMenu.setMnemonic(KeyEvent.VK_H); //Alt + H for help
+		loadItem.setMnemonic(KeyEvent.VK_L); //l for load
+		saveItem.setMnemonic(KeyEvent.VK_S); //s for save
+		exitItem.setMnemonic(KeyEvent.VK_E); //e for load
+		
+		fileMenu.add(loadItem);
+		fileMenu.add(saveItem);
+		fileMenu.add(exitItem);
+		
+		menuBar.add(fileMenu);
+		menuBar.add(helpMenu);
+		menuBar.add(editMenu);
+		
+		this.setJMenuBar(menuBar);
+		this.setVisible(true);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==loadItem) {
+			System.out.println("*beep boop* you loaded a file!");
+		}
+		if(e.getSource()==saveItem) {
+			System.out.println("*beep boop* you saved a file!");
+		}
+		if(e.getSource()==exitItem) {
+			System.exit(0);
+		}
+	}
+
+}
